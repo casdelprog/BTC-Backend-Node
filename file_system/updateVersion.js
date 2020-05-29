ab7d97ced73ad915ab3bacd02ec14f95648cef0d
Pablo Casas <casdelprog@gmail.com>
2020-05-29 17:16:38 +0200

    Creado fichero que aumenta la versión minor del package.json

--- file_system/updateVersion.js
+++ file_system/updateVersion.js
@@ -1,1 +1,19 @@
-console.log(process.env.npm_package_version);
+const fs = require("fs");
+const dataRead =fs.readFileSync('./package.json', 'utf-8');
+var dataJson = JSON.parse(dataRead);
+
+var versionNode = dataJson.version;
+console.log('Version original:', versionNode);
+
+const versionUpgrade = updgradeMinor(versionNode, '.')
+console.log('Minor incrementada:', versionUpgrade);
+
+dataJson.version = versionUpgrade;
+
+fs.writeFileSync("package.json", JSON.stringify(dataJson, null, 2));
+
+function updgradeMinor(versionStr, separator){
+    versionArray = versionStr.split(separator);
+    minorIncreased = parseInt(versionArray[1]) +1;
+    return versionArray[0] + separator + minorIncreased + separator + versionArray[2];
+}
