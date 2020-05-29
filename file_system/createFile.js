21a8c2a726209f7d9efa2461f57fe983fe62edf3
Pablo Casas <casdelprog@gmail.com>
2020-05-29 16:46:51 +0200

    Fin ejemplo lectura/escritura en fichero

--- file_system/createFile.js
+++ file_system/createFile.js
@@ -1,5 +1,14 @@
 const fs = require("fs");
 
-const data = JSON.stringify(process);
+const data = JSON.stringify(process, null, 2);
 
 fs.writeFileSync("data.json", data);
+
+const dataRead =fs.readFileSync('./data.json', 'utf-8');
+
+console.log(typeof dataRead);
+//console.log(dataRead);
+
+const jsonData = JSON.parse(dataRead);
+
+console.log(jsonData.version);
